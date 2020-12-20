baf1cfa3ba49b179c84e89880ca9b39fbf2d791a
yelinIT <yelinphyocu@gmail.com>
2020-12-20 16:07:18 +0630

    Initial commit

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="fluid-container footer">
+    <p class="text-center">Copyright &copy 2018, ABC Marketing. All Rights Reserved.</p>
+  </div>
+</template>
+
+<style scoped>
+.fluid-container.footer{
+  background: blue;
+}
+.fluid-container.footer > *:last-child {
+    margin-bottom: 0px;
+    color: #fff;
+}
+</style>
